a7cca3ada7cfc72ba29ba36490cde1116b9ffdb8
Matias Grioni <matgrioni@gmail.com>
2017-08-03 22:10:25 -0700

    Finalized bogzo style and documentation.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 A small repo for my curiosities. Kinda like a blog with all the cool side tangents but for programming.
 
 ### Directory
-- Bogzo: A comparison of bogo and bozo sort!
+- Bogzo: A comparison of bogo and bozo sort! It seems like they are roughly the same interestingly enough. (My prediction was that bogo would be better since it searches the state space more randomly). But try it for yourself and look at the graphs or tell me if my methodology or code is wrong.
--- bogzo.py
+++ bogzo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import collections
 import math
 import random
 import time
@@ -21,8 +22,8 @@ def main():
     parser.add_argument('--time', help='Provide an analysis of the time for each approach.', action='store_true')
     args = parser.parse_args()
 
-    bogo_results = {}
-    bozo_results = {}
+    bogo_results = new_results_store()
+    bozo_results = new_results_store()
 
     for n in xrange(args.down, args.up + 1):
         items = range(n)
@@ -33,16 +34,12 @@ def main():
             copy2 = items[:]
 
             iters, time = bogo(copy1)
-            try:
-                bogo_results[n].append(iters)
-            except KeyError:
-                bogo_results[n] = [iters]
+            bogo_results[n]['iters'].append(iters)
+            bogo_results[n]['times'].append(time)
 
             iters, time = bozo(copy2)
-            try:
-                bozo_results[n].append(iters)
-            except KeyError:
-                bozo_results[n] = [iters]
+            bozo_results[n]['iters'].append(iters)
+            bozo_results[n]['times'].append(time)
 
 
     if args.hist:
@@ -54,22 +51,33 @@ def main():
             plt.show()
 
     if args.avg:
-        diff = args.up - args.down + 1
-
         ns = range(args.down, args.up + 1)
-        bogo_avgs = [0] * diff
-        bozo_avgs = [0] * diff
-        for n, results in bogo_results.items():
-            bogo_avgs[n - args.down] = sum(results) / float(len(results))
+        bogo_avgs = average_metric(bogo_results, 'iters')
+        bogo_ys = continuous_keys_to_list(bogo_avgs, args.down, args.up)
 
-        for n, results in bozo_results.items():
-            bozo_avgs[n - args.down] = sum(results) / float(len(results))
+        bozo_avgs = average_metric(bozo_results, 'iters')
+        bozo_ys = continuous_keys_to_list(bozo_avgs, args.down, args.up)
 
-        plt.plot(ns, bogo_avgs, label='bogo')
-        plt.plot(ns, bozo_avgs, label='bozo')
+        plt.plot(ns, bogo_ys, label='bogo')
+        plt.plot(ns, bozo_ys, label='bozo')
         plt.yscale('log')
         plt.legend(loc='upper right')
-        plt.title('Bogo vs Bozo for all n')
+        plt.title('Bogo vs Bozo iters for all n')
+        plt.show()
+
+    if args.time:
+        ns = range(args.down, args.up + 1)
+        bogo_avgs = average_metric(bogo_results, 'times')
+        bogo_ys = continuous_keys_to_list(bogo_avgs, args.down, args.up)
+
+        bozo_avgs = average_metric(bozo_results, 'times')
+        bozo_ys = continuous_keys_to_list(bozo_avgs, args.down, args.up)
+
+        plt.plot(ns, bogo_ys, label='bogo')
+        plt.plot(ns, bozo_ys, label='bozo')
+        plt.yscale('log')
+        plt.legend(loc='upper right')
+        plt.title('Bogo vs Bozo time for all n')
         plt.show()
 
 
@@ -87,9 +95,68 @@ def timeit(method):
         result = method(*args, **kw)
         end = time.time()
 
-        return (result, start - end)
+        return (result, end - start)
 
     return timed
+
+
+def new_results_store():
+    """
+    Create a new dictionary to store the results of the two sort. The structure
+    of the store is designed so that on the first level, the key is the n for
+    which the simulations were run. The value is then a dict whose key is the
+    type of metric and the value is a list of the values of this metric for each
+    run of the sort.
+
+    Returns:
+        type(dict): A mutlilayered defaultdict so no annoying nested exceptions.
+    """
+    return collections.defaultdict(lambda: collections.defaultdict(list))
+
+
+def average_metric(results, metric):
+    """
+    Given a results dictionary as created in new_results_store, return the
+    average for a given metric name.
+
+    Args:
+        results, type(dict): The results store.
+        metric, type(string): The name of the metric.
+
+    Returns:
+        type(dict): An average for the desired metric from the results for each
+                    available n.
+    """
+    avgs = {}
+    for n, simulations in results.items():
+        metrics = simulations[metric]
+        avgs[n] = sum(metrics) / float(len(metrics))
+
+    return avgs
+
+
+def continuous_keys_to_list(d, down, up):
+    """
+    Converts a dictionary with integer keys that are continuously increasing, eg
+    1, 2, 3, 4..., into a list in increasing order. The list is 0 based no matter
+    the dictionary keys but it keeps the order and starts from down and ends at
+    up.
+
+    Args:
+        d, type(dict): The dictionary to convert into a list.
+        down, type(int): The starting key value.
+        up, type(int): The ending key value.
+
+    Returns:
+        type(list): A list corresponding to the values for the keys from
+                    [down, up] in ascending key order.
+    """
+    diff = up - down + 1
+    ys = [0] * diff
+    for i in xrange(down, up + 1):
+        ys[i - down] = d[i]
+
+    return ys
 
 
 @timeit
